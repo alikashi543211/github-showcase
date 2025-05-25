32b8a1cfb84a5a43a56a40f7d954cfa9a18c150f
alikashi543211 <alikashi54321@gmail.com>
2025-05-26 02:08:26 +0500

    footer shou hide

--- resources/views/front/includes/footer.blade.php
+++ resources/views/front/includes/footer.blade.php
@@ -1,12 +1,15 @@
 <!-- Footer Section -->
-<footer class="bg-light border-top py-4">
-    <div class="container text-center">
-        <p class="mb-2 text-muted fw-semibold mb-1">
-            {!! $settings['github_showcase_footer_copyright_text'] ?? '-----' !!}
-        </p>
-        <a href="{{ $settings['github_showcase_footer_website_link_text'] ?? '#' }}" target="_blank"
-            class="text-primary text-decoration-none fw-medium">
-            {{ $settings['github_showcase_footer_website_link_text'] ?? '-----' }}
-        </a>
-    </div>
-</footer>
+@if (isset($settings['is_github_showcase_footer_show_hide']) &&
+        $settings['is_github_showcase_footer_show_hide'] == 'Yes')
+    <footer class="bg-light border-top py-4">
+        <div class="container text-center">
+            <p class="mb-2 text-muted fw-semibold mb-1">
+                {!! $settings['github_showcase_footer_copyright_text'] ?? '-----' !!}
+            </p>
+            <a href="{{ $settings['github_showcase_footer_website_link_text'] ?? '#' }}" target="_blank"
+                class="text-primary text-decoration-none fw-medium">
+                {{ $settings['github_showcase_footer_website_link_text'] ?? '-----' }}
+            </a>
+        </div>
+    </footer>
+@endif
