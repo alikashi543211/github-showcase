26f364231e4454f4d647c63fb195f113c4943015
alikashi543211 <alikashi54321@gmail.com>
2025-06-05 21:06:55 +0500

    github showcase

--- resources/views/front/includes/footer.blade.php
+++ resources/views/front/includes/footer.blade.php
@@ -6,7 +6,7 @@
             <p class="mb-2 text-muted fw-semibold mb-1">
                 {!! $settings['github_showcase_footer_copyright_text'] ?? '-----' !!}
             </p>
-            <a href="{{ $settings['github_showcase_footer_website_link_text'] ?? '#' }}" target="_blank"
+            <a href="{{ $settings['github_showcase_footer_website_link'] ?? '#' }}" target="_blank"
                 class="text-primary text-decoration-none fw-medium">
                 {{ $settings['github_showcase_footer_website_link_text'] ?? '-----' }}
             </a>
